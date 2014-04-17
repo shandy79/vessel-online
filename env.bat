e95ced68fc33d1765b3884e674b97f41d6aeee89
steve.handy@gmail.com <steve.handy@gmail.com@eff0a031-7a33-0686-e7e6-ef33ef888833>
2014-04-17 12:56:51 +0000

    Updates for various Java versions, new environment

--- env.bat
+++ env.bat
@@ -1,38 +1,49 @@
 @echo off
 rem http://technet.microsoft.com/en-us/library/bb490954.aspx
 
-set SOFTWARE_LIB=C:\Users\handy\Software
-set ANT_BIN=%SOFTWARE_LIB%\eclipse-juno\plugins\org.apache.ant_1.8.3.v20120321-1730\bin
-set CVS_BIN=%SOFTWARE_LIB%\cvs-1.11.22
-set DIFF_BIN=%SOFTWARE_LIB%\diffutils-2.8.7\bin
-set GREP_BIN=%SOFTWARE_LIB%\grep-2.5.4\bin
-set PUTTY_BIN=%SOFTWARE_LIB%\StandaloneExe\putty-0.62
-set SOAPUI_BIN=%SOFTWARE_LIB%\StandaloneExe\soapui-4.5.2\bin
-set UNISON_BIN=%SOFTWARE_LIB%\unison-2.32.52
-set WGET_BIN=%SOFTWARE_LIB%\wget-1.11.4\bin
+set SOFTWARE_LIB=C:\Users\shandy3\Software
+REM set ANT_BIN=%SOFTWARE_LIB%\eclipse-juno\plugins\org.apache.ant_1.8.3.v20120321-1730\bin
+set CURL_BIN=%SOFTWARE_LIB%\curl-7.33.0
+REM set CVS_BIN=%SOFTWARE_LIB%\cvs-1.11.22
+REM set DIFF_BIN=%SOFTWARE_LIB%\diffutils-2.8.7\bin
+set GRAILS_BIN=%SOFTWARE_LIB%\grails-2.3.7\bin
+REM set GREP_BIN=%SOFTWARE_LIB%\grep-2.5.4\bin
+set PUTTY_BIN=C:\Program Files (x86)\PuTTY
+REM set SOAPUI_BIN=%SOFTWARE_LIB%\StandaloneExe\soapui-4.5.2\bin
+REM set SVN_BIN=C:\Program Files\TortoiseSVN\bin
+set SVN_BIN=C:\Program Files\SlikSvn\bin
+REM set UNISON_BIN=%SOFTWARE_LIB%\unison-2.32.52
+REM set WGET_BIN=%SOFTWARE_LIB%\wget-1.11.4\bin
 
-set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_35
+set JAVA_HOME=C:\Program Files (x86)\Java\jre7
 set JAVA_BIN=%JAVA_HOME%\bin
 
-set M2_HOME=%SOFTWARE_LIB%\apache-maven-3.0.4
-set M2=%M2_HOME%\bin
+set JDK_HOME=C:\Program Files (x86)\Java\jdk1.7.0_55
+set JDK_BIN=%JDK_HOME%\bin
+
+set JAVA64_HOME=C:\Program Files\Java\jre7
+set JAVA64_BIN=%JAVA64_HOME%\bin
+
+set JDK64_HOME=C:\Program Files\Java\jdk1.7.0_25
+set JDK64_BIN=%JDK64_HOME%\bin
+
+REM set M2_HOME=%SOFTWARE_LIB%\apache-maven-3.0.4
+REM set M2=%M2_HOME%\bin
 
 set PYTHON_HOME=C:\Python27
 set PYTHON_SCRIPTS=%PYTHON_HOME%\Scripts
 
 set ORIG_PATH=%PATH%
-set PATH=%ANT_BIN%;%CVS_BIN%;%DIFF_BIN%;%GREP_BIN%;%PUTTY_BIN%;%SOAPUI_BIN%;%UNISON_BIN%;%WGET_BIN%;%JAVA_BIN%;%M2%;%PYTHON_HOME%;%PYTHON_SCRIPTS%;%PATH%
+REM set PATH=%ANT_BIN%;%CVS_BIN%;%DIFF_BIN%;%GREP_BIN%;%PUTTY_BIN%;%SOAPUI_BIN%;%UNISON_BIN%;%WGET_BIN%;%JAVA_BIN%;%M2%;%PYTHON_HOME%;%PYTHON_SCRIPTS%;%PATH%
+set PATH=%CURL_BIN%;%GRAILS_BIN%;%PUTTY_BIN%;%SVN_BIN%;%JAVA_BIN%;%PYTHON_HOME%;%PYTHON_SCRIPTS%;%PATH%
 
 set HOME=%HOMEDRIVE%%HOMEPATH%
-set PRDS=C:\PRDS
 
 doskey cdhome=cd %HOME%
 doskey cddesk=cd %HOME%\Desktop
 doskey cddocs=cd %HOME%\Documents
 doskey cdprog32=cd %PROGRAMFILES(X86)%
 doskey cdprog64=cd %PROGRAMFILES%
-doskey cdprds=cd %PRDS%
-doskey cdbuild=cd %PRDS%\Build
 doskey cdsoft=cd %SOFTWARE_LIB%
 
 doskey cat=type $*
@@ -45,5 +56,7 @@ doskey mv=move $*
 doskey pwd=echo %CD%
 doskey rm=del $*
 
-set CVS_RSH=%PUTTY_BIN%\PLINK.EXE
+doskey grails231=%SOFTWARE_LIB%\grails-2.3.1\bin\grails $*
+doskey grails234=%SOFTWARE_LIB%\grails-2.3.4\bin\grails $*
 
+REM set CVS_RSH=%PUTTY_BIN%\PLINK.EXE
